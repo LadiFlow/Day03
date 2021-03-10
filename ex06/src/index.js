d4f06fb893fc5970f00612a8fccbbb274fab8387
LadiFlow <adi_curt@hotmail.com>
2021-03-10 19:18:29 +0100

    Added index.js

---
+++ ex06/src/index.js
@@ -0,0 +1,13 @@
+function main() {
+    // Only change code below this line
+    var x;
+    var y;
+    var z;
+    x = 9;
+    y = 16;
+    z = "My name is John!";
+    // Only change code above this line
+    return [x, y, z];
+}
+main();
+module.exports = main;
